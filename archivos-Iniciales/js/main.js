a253a3bef8554be5658c454702152ae9cb1fd8d7
juani sorato <juanisorato17@yahoo.com>
2023-06-26 19:32:32 -0300

    Subo repo de archivos iniciales

---
+++ archivos-Iniciales/js/main.js
@@ -0,0 +1,137 @@
+// PRODUCTOS
+const productos = [
+    // Celulares
+    {
+        id: "celular-01",
+        titulo: "iPhoen 14 pro max",
+        imagen: "./img/celulares/01.jpg",
+        categoria: {
+            nombre: "Celulares",
+            id: "celulares"
+        },
+        precio: 1099
+    },
+    {
+        id: "celular-02",
+        titulo: "Samsung S23",
+        imagen: "./img/celular/02.jpg",
+        categoria: {
+            nombre: "Celulares",
+            id: "celulares"
+        },
+        precio: 500
+    },
+    {
+        id: "celular-03",
+        titulo: "iPhone x",
+        imagen: "./img/celular/03.jpg",
+        categoria: {
+            nombre: "Celulares",
+            id: "celulares"
+        },
+        precio: 700
+    },
+    {
+        id: "celular-04",
+        titulo: "Samsung A34 ",
+        imagen: "./img/celulares/04.jpg",
+        categoria: {
+            nombre: "Celulares",
+            id: "celulares"
+        },
+        precio: 300
+    },
+    {
+        id: "celular-05",
+        titulo: "Moto edge 30",
+        imagen: "./img/celulares/05.jpg",
+        categoria: {
+            nombre: "Celulares",
+            id: "celulares"
+        },
+        precio: 450
+    },
+    // Cargador
+    {
+        id: "cargador-01",
+        titulo: "Samsung carg 25w",
+        imagen: "./img/cargadores/01.jpg",
+        categoria: {
+            nombre: "Cargadores",
+            id: "cargadores"
+        },
+        precio: 10
+    },
+    {
+        id: "cargador-02",
+        titulo: "Motorola carg 20w",
+        imagen: "./img/cargadores/02.jpg",
+        categoria: {
+            nombre: "Cargadores",
+            id: "cargadores"
+        },
+        precio: 9
+    },
+    {
+        id: "cargador-03",
+        titulo: "Motorola carg 30w",
+        imagen: "./img/cargadores/03.jpg",
+        categoria: {
+            nombre: "Cargadores",
+            id: "cargadores"
+        },
+        precio: 18
+    },
+    {
+        id: "cargador-04",
+        titulo: "Motorola carg 50w",
+        imagen: "./img/cargadores/04.jpg",
+        categoria: {
+            nombre: "Cargadores",
+            id: "cargadores"
+        },
+        precio: 25
+    },
+    {
+        id: "cargador-05",
+        titulo: "belkin 30w",
+        imagen: "./img/cargadores/05.jpg",
+        categoria: {
+            nombre: "Cargadores",
+            id: "cargadores"
+        },
+        precio: 15
+    },
+   
+    // Cables
+    {
+        id: "cables-01",
+        titulo: "Cable lightning ",
+        imagen: "./img/cables/01.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 3
+    },
+    {
+        id: "cables-02",
+        titulo: "Cable usb-c",
+        imagen: "./img/cables/02.jpg",
+        categoria: {
+            nombre: "Cables",
+            id: "cables"
+        },
+        precio: 3
+    },
+    {
+        id: "cables-03",
+        titulo: "Cable mini-usb",
+        imagen: "./img/cables/03.jpg",
+        categoria: {
+            nombre: "Cables",
+            id: "cables"
+        },
+        precio: 3
+    }
+];
